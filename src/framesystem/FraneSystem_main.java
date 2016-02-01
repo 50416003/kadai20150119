2c402aadf1cb56aae0336b88bc8bb3170a3e6a58
50416003 <sinon0428@gmail.com>
2016-02-01 19:00:50 +0900

    20150126 submit

---
+++ src/framesystem/FraneSystem_main.java
@@ -0,0 +1,13 @@
+package framesystem;
+
+public class FraneSystem_main {
+public static void main(String [] args){
+FrameSystem_view frame = new FrameSystem_view();
+
+frame.setBounds(5,5,655,455);
+
+frame.setVisible(true);
+
+
+}
+}
